17d75f5d8a259879176973ec996722120eb82791
bwuphan <bwuphan@gmail.com>
2019-08-04 20:57:00 -0700

    Add basic functionality to src/components/Products/index.js

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { withFirebase } from '../Firebase';
 
+import ProductModal from '../ProductModal';
 import Loader from '../Loader';
 import data from '../../resource/data.js';
 
@@ -19,68 +20,44 @@ class Products extends Component {
 
   componentDidMount() {
     this.setState({ products: JSON.parse(data).groups });
-    console.log(JSON.parse(data));
-    // function getJSON(url, qs_params) {
-    //   function buildQueryString(params) {
-    //     return Object.entries(params).map(d => `${d[0]}=${d[1]}`).join('&');
-    //   }
-
-    //   return new Promise((resolve, reject) => {
-    //     const qs = qs_params ? '?' + buildQueryString(qs_params) : '';
-    //     const xhr = new XMLHttpRequest();
-    //     xhr.open('GET', `${url}${qs}`);
-
-    //     xhr.onload = function() {
-    //       if (xhr.status >= 200 && xhr.status < 400) {
-    //         resolve(JSON.parse(xhr.responseText));
-    //       } else {
-    //         resolve(xhr.responseText);
-    //       }
-    //     };
-    //     xhr.onerror = () => reject(xhr.statusText);
-    //     xhr.send();
-    //   });
-    // }
-    // console.log('here');
-    // getJSON('https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json').then(results => console.log(results));'
-
-    // this.setState({ loading: true });
-
-    // this.props.firebase.Products().on('value', snapshot => {
-    //   const messagesObject = snapshot.val();
-
-    //   const Products = Object.keys(messagesObject).map(key => messagesObject[key]).reverse();
-    //   this.setState({ Products, loading: false })
-    // });
-
-    // this.props.firebase.users().on('value', snapshot => {
-    //   const usersObject = snapshot.val();
-
-    //   this.setState({
-    //     users: usersObject,
-    //     loading: false
-    //   });
-    // });
   }
 
   componentWillUnmount() {
     this.props.firebase.users().off();
-    // this.props.firebase.Products().off();
   }
 
+  groupProducts = (products, num) =>
+    products.reduce((newArr, prod, i) => {
+      if (i % num === 0) newArr.push([prod]);
+      else newArr[newArr.length - 1].push(prod);
+      return newArr;
+    }, []);
+
   render() {
-    const { products } = this.state;
+    let { products } = this.state;
     if (products) {
+      products = this.groupProducts(products, 3);
       return (
-        <div>
-          {products.map((product, i) =>
-            <div className="card" style={{'width': '18rem'}} key={i}>
-              <img className="card-img-top" src={product.hero.href} alt="Card image cap"></img>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
+        <div className="d-flex flex-column justify-content-center">
+          {products.map((group, i) =>
+            <div className="products__group d-flex" key={i}>
+              {group.map((product, j) =>
+                <div className="p-3 d-flex justify-content-center">
+                  <div className="card height-31 width-20 d-flex align-content-between" key={j}>
+                    <img className="card-img-top" src={product.hero.href} alt="Card image cap"></img>
+                    <div className="card-body">
+                      <h6
+                        onClick={this.groupProducts}
+                        className="card-title text-center"
+                      >{product.name}</h6>
+                      <p className="card-text text-success font-weight-bold text-center">
+                        From ${product.priceRange.selling.low} - ${product.priceRange.selling.high}
+                      </p>
+                    </div>
+                    <ProductModal product={product}/>
+                  </div>
+                </div>
+              )}
             </div>
           )}
         </div>
