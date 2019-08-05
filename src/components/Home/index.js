2d0a685b0aa571d26c6046c0673302d127a02f73
bwuphan <bwuphan@gmail.com>
2019-08-04 22:32:56 -0700

    Add background color to src/components/Home/index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Products from '../Products';
 const HomePage = () => (
-  <div>
+  <div style={{'backgroundColor': '#36454f'}}>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
     <Products />
