91ac4ae391b4c654eda160dbcb7738ee85c56e92
bwuphan <bwuphan@gmail.com>
2019-08-04 22:32:29 -0700

    Add navbar to src/components/App/index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-// import Navigation from '../Navigation';
+import Navigation from '../Navigation';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
 import Home from '../Home';
+// import CartPage from '../Cart';
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
     <div>
+      <Navigation />
       <Route exact path={ROUTES.HOME} component={Home} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
@@ -26,3 +28,4 @@ const App = () => (
 );
 
 export default withAuthentication(App);
+      // <Route path={ROUTES.CART} component={CartPage} />
