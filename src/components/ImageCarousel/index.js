bf3346c1d43d7c94abee33af7a5e2f29dd48019e
bwuphan <bwuphan@gmail.com>
2019-08-04 22:33:20 -0700

    Make ImageCarousel a functional stateless component

--- src/components/ImageCarousel/index.js
+++ src/components/ImageCarousel/index.js
@@ -1,41 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 import Loader from '../Loader';
 
-class ImageCarousel extends Component {
-  constructor(props) {
-    super(props);
+const imageCarousel = ({ images }) => {
+  if (images && images.length > 0) {
+    return (
+      <Carousel>
+        {images.map((image, i) =>
+        <Carousel.Item key={i}>
+          <img
+            className="d-block w-100"
+            src={image.href}
+            alt={image.alt}
+          />
+        </Carousel.Item>
+        )}
+      </Carousel>
+    );
+  } else {
+    return (
+      <div className="text-center">
+        <Loader color="primary"/>
+      </div>
+    );
+  };
+};
 
-    this.state = {
-      visible: false
-    }
-  }
-
-  toggleModal = () => {
-    this.setState(prevState => ({ visible: !prevState.visible }));
-  }
-
-  render() {
-    const images = this.props.images;
-    if (images && images.length > 0) {
-      return (
-        <Carousel>
-          {images.map((image, i) =>
-          <Carousel.Item key={i}>
-            <img
-              className="d-block w-100"
-              src={image.href}
-              alt={image.alt}
-            />
-          </Carousel.Item>
-          )}
-        </Carousel>
-      );
-    } else {
-      return (<div className="text-center"><Loader color="primary"/></div>)
-    }
-  }
-}
-
-export default ImageCarousel;
+export default imageCarousel;
