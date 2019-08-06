cc43d7003bbb83b089de0b1784fe27962fad54dc
bwuphan <bwuphan@gmail.com>
2019-08-05 23:14:05 -0700

    Add fuctionality to src/components/Cart/index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,4 +1,77 @@
-import CartContext from './context';
-import withCart from './withCart';
+import React, { Component } from 'react';
 
-export { CartContext, withCart };
+import { withFirebase } from '../Firebase';
+import Loader from '../Loader';
+
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      username: '',
+      cart: []
+    }
+
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true })
+    this.props.firebase.user(this.props.authUser.uid).on('value', snapshot => {
+      const userObject = snapshot.val();
+
+      const cart = Object.keys(userObject.cart).map(id => {
+        return {
+          quantity: userObject.cart[id],
+          id: id
+        }
+      });
+      this.setState({
+        username: userObject.username,
+        loading: false,
+        cart
+      });
+    });
+  };
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.authUser.uid).off();
+  };
+
+  checkout = () => {
+    this.props.firebase.doUpdateCart(false, this.props.authUser.uid)
+      .then(() => alert('thank you for your purchase!'));
+  };
+
+  render() {
+    const { username, cart, loading } = this.state;
+    if (!loading) {
+      return (
+      <div className="container pt-5">
+        <h2 className="text-center card-header border">{username}'s Cart</h2>
+          <table className="table table-bordered table-striped w-100">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map(item => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button className="btn btn-success" onClick={this.checkout} disabled={!cart.length}>Check Out</button>
+        </div>
+      );
+    } else {
+      return <Loader />
+    };
+  };
+};
+
+export default withFirebase(Cart);
