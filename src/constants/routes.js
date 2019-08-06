86631da89403b6df5d5b14ad89ae8950a7fb65d2
bwuphan <bwuphan@gmail.com>
2019-08-05 23:17:31 -0700

    Update routes in routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,5 +2,6 @@ export const HOME = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
+export const CART = '/cart';
+export const CHECKOUT = '/checkout';
