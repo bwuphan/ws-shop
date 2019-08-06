40067d3fe05e0f75f0e36c332e362ec4b904dd28
bwuphan <bwuphan@gmail.com>
2019-08-05 23:15:06 -0700

    Update styling in src/components/Product/index.js

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import ProductModal from '../ProductModal';
-import Loader from '../Loader';
 
-const product = ({ product, idx }) => (
+const product = ({ product, idx, addToCart, isSignedIn }) => (
   <div className="p-3 d-flex justify-content-center" key={idx}>
-    <div className="card height-31 width-20 d-flex align-content-between">
-      <img className="card-img-top" src={product.hero.href} alt="Card image cap"></img>
+    <div className="card height-31 width-20 d-flex align-content-between shadow-sm">
+      <img className="card-img-top" src={product.hero.href} alt={`${product.name}`}></img>
       <div className="card-body">
         <h6 className="card-title text-center">{product.name}</h6>
         <p className="card-text text-success font-weight-bold text-center">
           From ${product.priceRange.selling.low} - ${product.priceRange.selling.high}
         </p>
       </div>
+      <div className="d-flex justify-content-around pb-2">
+      {isSignedIn && <button className="btn btn-primary w-25" onClick={() => addToCart(product.id)}>Add</button>}
       <ProductModal product={product}/>
+      </div>
     </div>
   </div>
 );
